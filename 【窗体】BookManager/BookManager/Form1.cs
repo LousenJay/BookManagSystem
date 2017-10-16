6c1950f8abd15014501230adccc8422d44d43eb9
LousenJay <lousenjay@gmail.com>
2017-10-16 21:34:40 +0800

    【窗体】BookManager

---
+++ 【窗体】BookManager/BookManager/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookManager
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*添加子窗口2*/
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 frm2 = new Form2();
+            frm2.Show();
+        }
+        /*添加子窗口3*/
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form3 frm3 = new Form3();
+            frm3.Show();
+        }
+        /*添加子窗口4*/
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form4 frm4= new Form4();
+            frm4.Show();
+        }
+        /*添加子窗口5*/
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form5 frm5 = new Form5();
+            frm5.Show();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ 【窗体】BookManager/BookManager/Form2.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace BookManager
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.label1 = new System.Windows.Forms.Label();
+            this.BID = new System.Windows.Forms.TextBox();
+            this.BName = new System.Windows.Forms.TextBox();
+            this.BAuthor = new System.Windows.Forms.TextBox();
+            this.Bpublish = new System.Windows.Forms.TextBox();
+            this.Bprice = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "请输入新增书籍的信息：";
+            // 
+            // BID
+            // 
+            this.BID.Location = new System.Drawing.Point(168, 79);
+            this.BID.Name = "BID";
+            this.BID.Size = new System.Drawing.Size(164, 21);
+            this.BID.TabIndex = 1;
+            // 
+            // BName
+            // 
+            this.BName.Location = new System.Drawing.Point(168, 141);
+            this.BName.Name = "BName";
+            this.BName.Size = new System.Drawing.Size(164, 21);
+            this.BName.TabIndex = 2;
+            // 
+            // BAuthor
+            // 
+            this.BAuthor.Location = new System.Drawing.Point(168, 192);
+            this.BAuthor.Name = "BAuthor";
+            this.BAuthor.Size = new System.Drawing.Size(164, 21);
+            this.BAuthor.TabIndex = 3;
+            // 
+            // Bpublish
+            // 
+            this.Bpublish.Location = new System.Drawing.Point(168, 245);
+            this.Bpublish.Name = "Bpublish";
+            this.Bpublish.Size = new System.Drawing.Size(164, 21);
+            this.Bpublish.TabIndex = 4;
+            // 
+            // Bprice
+            // 
+            this.Bprice.Location = new System.Drawing.Point(168, 298);
+            this.Bprice.Name = "Bprice";
+            this.Bprice.Size = new System.Drawing.Size(164, 21);
+            this.Bprice.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(95, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 19);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "编号:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(95, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 19);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "书名：";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(95, 192);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 19);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "作者:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(76, 245);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 19);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "出版社：";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Location = new System.Drawing.Point(95, 298);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 19);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "价格：";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(178, 394);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "确定";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::BookManager.Properties.Resources.边框6;
+            this.ClientSize = new System.Drawing.Size(436, 476);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Bprice);
+            this.Controls.Add(this.Bpublish);
+            this.Controls.Add(this.BAuthor);
+            this.Controls.Add(this.BName);
+            this.Controls.Add(this.BID);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "新增";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox BID;
+        private System.Windows.Forms.TextBox BName;
+        private System.Windows.Forms.TextBox BAuthor;
+        private System.Windows.Forms.TextBox Bpublish;
+        private System.Windows.Forms.TextBox Bprice;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+    }
+}
